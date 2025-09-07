68caf63c58ff8053c16543ff55efa188e307e7e9
Nitin Nazeer <nitin.nazeer@gmail.com>
2025-09-07 16:26:17 -0700

    Use tab to traverse snippets and tabstops

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -13,12 +13,13 @@ return {
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+						luasnip.lsp_expand(args.body) -- For `luasnip` users.
 					end,
 				},
 				window = {
@@ -31,6 +32,27 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					-- Expand or jump to the next tab stop with Tab
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }), -- 'i' for Insert mode, 's' for Select mode (visual selection inside a snippet)
+
+					-- Jump to the previous tab stop with Shift-Tab
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
